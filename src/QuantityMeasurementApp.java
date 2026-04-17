43410a85d88b6972f5a4a9d087ffe92f20f4ac48
al30z <arushilakhera30@gmail.com>
2026-04-17 15:38:59 +0530

    UC2:Feet and Inches Measurement

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,14 +1,36 @@
-public class QuantityMeasurementApp {
+class Feet {
+    private final double value;
 
-    public static boolean compareFeet(double value1, double value2) {
-        return Double.compare(value1, value2) == 0;
+    public Feet(double value) {
+        this.value = value;
     }
 
-    public static void main(String[] args) {
-        double feet1 = 3.0;
-        double feet2 = 3.0;
+    public boolean equals(Feet other) {
+        return Double.compare(this.value, other.value) == 0;
+    }
+}
 
-        boolean result = compareFeet(feet1, feet2);
-        System.out.println("Feet equality result: " + result);
+class Inches {
+    private final double value;
+
+    public Inches(double value) {
+        this.value = value;
+    }
+
+    public boolean equals(Inches other) {
+        return Double.compare(this.value, other.value) == 0;
+    }
+}
+
+public class QuantityMeasurementApp {
+
+    public static void main(String[] args) {
+        Feet feet1 = new Feet(3.0);
+        Feet feet2 = new Feet(3.0);
+        System.out.println("Feet equality: " + feet1.equals(feet2));
+
+        Inches inch1 = new Inches(12.0);
+        Inches inch2 = new Inches(12.0);
+        System.out.println("Inches equality: " + inch1.equals(inch2));
     }
 }
