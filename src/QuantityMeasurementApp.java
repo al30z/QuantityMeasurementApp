afd92268b9a9cb972118c82b1283990ec69dc81a
al30z <arushilakhera30@gmail.com>
2026-04-17 15:46:29 +0530

    UC4:Extended unit support

--- src/QuantityMeasurementApp.java
+++ src/QuantityMeasurementApp.java
@@ -1,6 +1,8 @@
 enum LengthUnit {
-    FEET(12.0),   // 1 foot = 12 inches
-    INCH(1.0);    // base unit is inch
+    FEET(12.0),          // 1 foot = 12 inches
+    INCH(1.0),           // base unit is inch
+    YARD(36.0),          // 1 yard = 36 inches
+    CM(0.393701);        // 1 cm = 0.393701 inches
 
     private final double conversionFactor;
 
@@ -31,9 +33,12 @@ class QuantityLength {
 
 public class QuantityMeasurementApp {
     public static void main(String[] args) {
-        QuantityLength feet = new QuantityLength(1.0, LengthUnit.FEET);
-        QuantityLength inches = new QuantityLength(12.0, LengthUnit.INCH);
+        QuantityLength yard = new QuantityLength(1.0, LengthUnit.YARD);
+        QuantityLength feet = new QuantityLength(3.0, LengthUnit.FEET);
+        QuantityLength cm = new QuantityLength(2.54, LengthUnit.CM);
+        QuantityLength inch = new QuantityLength(1.0, LengthUnit.INCH);
 
-        System.out.println("Feet equals Inches: " + feet.equals(inches));
+        System.out.println("1 Yard equals 3 Feet: " + yard.equals(feet));
+        System.out.println("2.54 cm equals 1 Inch: " + cm.equals(inch));
     }
 }
